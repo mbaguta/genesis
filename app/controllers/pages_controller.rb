bd3fa7f8a0a16c18a3fb18874628af241084217d
mbaguta <hmbaguta@yahoo.com>
2015-07-23 10:41:05 -0700

    done static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,18 @@
 class PagesController < ApplicationController
+	layout 'application'
   def home
+  	@title = "Home"
   end
 
+
+def about
+	 @title = "About"
+  end
+
+
   def contact
+  	@title = "Contact"
   end
 end
+
+#instance variables @title
